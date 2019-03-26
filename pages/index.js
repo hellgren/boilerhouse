6ae3e0179de0669b68a6277871204dc399678f56
Jonas Hellgren <jonas.hellgren@metro.se>
2019-03-26 13:35:19 +0100

    Add styled component example

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
-export default () => {
-  return "Hello world";
+import styled from "@emotion/styled";
+
+const TextWrapper = styled.div`
+  text-decoration: underline;
+`;
+
+const Homepage = () => {
+  return <TextWrapper>Hello World!</TextWrapper>;
 };
+
+export default Homepage;
