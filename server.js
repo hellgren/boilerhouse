f57971c2e4638ed8d917190a5c1085a6b42a9e0e
Jonas Hellgren <jonas.hellgren@metro.se>
2019-03-26 13:53:20 +0100

    Add express server, with compression, cache-headers, and static resources

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const next = require("next");
+const compression = require("compression");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.use(compression());
+
+    server.get(/^\/static\//, (_, res, nextHandler) => {
+      res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
+      nextHandler();
+    });
+
+    server.get("/robots.txt", (req, res) => {
+      const robotsOptions = {
+        root: `${__dirname}/static/`,
+        headers: {
+          "Content-Type": "text/plain;charset=UTF-8"
+        }
+      };
+      res.status(200).sendFile("robots.txt", robotsOptions);
+    });
+
+    server.get("/favicon.ico", (req, res) => {
+      const faviconOptions = {
+        root: `${__dirname}/static/`
+      };
+      res.status(200).sendFile("favicon.ico", faviconOptions);
+    });
+
+    server.get("*", (req, res) => handle(req, res));
+
+    server.listen(3000, err => {
+      if (err) throw err;
+      // eslint-disable-next-line no-console
+      console.log(
+        `> Ready on http://localhost:3000 - NODE_ENV=${process.env.NODE_ENV}.`
+      );
+    });
+  })
+  .catch(error => {
+    // eslint-disable-next-line no-console
+    console.error(error.stack);
+    process.exit(1);
+  });
