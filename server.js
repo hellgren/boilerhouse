2a663c61928aa2336aef92819c1973d3affb878c
Jonas Hellgren <jonas.hellgren@metro.se>
2019-03-27 12:25:14 +0100

    More heroku wrangling 😒

--- server.js
+++ server.js
@@ -37,11 +37,13 @@ app
 
     server.get("*", (req, res) => handle(req, res));
 
-    server.listen(3000, err => {
+    server.listen(process.env.port || 3000, err => {
       if (err) throw err;
       // eslint-disable-next-line no-console
       console.log(
-        `> Ready on http://localhost:3000 - NODE_ENV=${process.env.NODE_ENV}.`
+        `> Ready on http://localhost:§{process.env.port} - NODE_ENV=${
+          process.env.NODE_ENV
+        }.`
       );
     });
   })
